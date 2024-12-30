f24f043a45e5c462769694bbac1a08dcfde19191
dvz <32396582+dvz92@users.noreply.github.com>
2024-12-30 15:46:17 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -44,7 +44,7 @@ footer {
     margin-bottom: 4px;
     .description {
       &::after {
-        content: "@ Copyright by";
+        content: "© Copyright by";
         margin: 0 6px;
       }
     }
